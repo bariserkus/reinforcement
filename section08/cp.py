abf3056add9e9bb76aeb3c4af309aaa17a5ecd74
Baris Erkus <bariserkus@gmail.com>
2023-09-23 13:17:34 +0300

    Cartpole

--- section08/cp.py
+++ section08/cp.py
@@ -1,16 +1,22 @@
+# https://deeplearningcourses.com/c/artificial-intelligence-reinforcement-learning-in-python
+# https://www.udemy.com/artificial-intelligence-reinforcement-learning-in-python
 from __future__ import print_function, division
 from builtins import range
+# Note: you may need to update your version of future
+# sudo pip install -U future
 
-import numpy as np
 import gymnasium as gym
+import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.kernel_approximation import Nystroem, RBFSampler
+from sklearn.kernel_approximation import RBFSampler
 
 GAMMA = 0.99
 ALPHA = 0.1
 
 
-def eps_greedy(model, s, eps=0.1):
+def epsilon_greedy(model, s, eps=0.1):
+    # we'll use epsilon-soft to ensure all states are visited
+    # what happens if you don't do this? i.e. eps=0
     p = np.random.random()
     if p < (1 - eps):
         values = model.predict_all_actions(s)
@@ -35,14 +41,15 @@ def gather_samples(env, n_episodes=10000):
 
 
 class Model:
-    def __init__(self, e):
-        self.env = e
-        samples = gather_samples(e)
-        self.featurizer = Nystroem()
-        # self.featurizer = RBFSampler()
+    def __init__(self, env):
+        # fit the featurizer to data
+        self.env = env
+        samples = gather_samples(env)
+        self.featurizer = RBFSampler()
         self.featurizer.fit(samples)
         dims = self.featurizer.n_components
 
+        # initialize linear model weights
         self.w = np.zeros(dims)
 
     def predict(self, s, a):
@@ -67,7 +74,7 @@ def tagent(model, env, n_episodes=20):
         episode_reward = 0
         s, info = env.reset()
         while not (done or truncated):
-            a = eps_greedy(model, s, eps=0)
+            a = epsilon_greedy(model, s, eps=0)
             s, r, done, truncated, info = env.step(a)
             episode_reward += r
         reward_per_episode[it] = episode_reward
@@ -80,20 +87,23 @@ def watch_agent(model, env, eps):
     episode_reward = 0
     s, info = env.reset()
     while not (done or truncated):
-        a = eps_greedy(model, s, eps=eps)
+        a = epsilon_greedy(model, s, eps=eps)
         s, r, done, truncated, info = env.step(a)
         episode_reward += r
     print("Episode reward:", episode_reward)
 
 
 if __name__ == '__main__':
+    # instantiate environment
     env = gym.make("CartPole-v1", render_mode="rgb_array")
 
     model = Model(env)
     reward_per_episode = []
 
+    # watch untrained agent
     watch_agent(model, env, eps=0)
 
+    # repeat until convergence
     n_episodes = 1500
     for it in range(n_episodes):
         s, info = env.reset()
@@ -101,22 +111,27 @@ if __name__ == '__main__':
         done = False
         truncated = False
         while not (done or truncated):
-            a = eps_greedy(model, s)
+            a = epsilon_greedy(model, s)
             s2, r, done, truncated, info = env.step(a)
 
+            # get the target
             if done:
                 target = r
             else:
                 values = model.predict_all_actions(s2)
                 target = r + GAMMA * np.max(values)
 
-            grad = model.grad(s, a)
+            # update the model
+            g = model.grad(s, a)
             err = target - model.predict(s, a)
-            model.w += ALPHA * err * grad
+            model.w += ALPHA * err * g
 
+            # accumulate reward
             episode_reward += r
 
+            # update state
             s = s2
+
         if (it + 1) % 50 == 0:
             print(f"Episode: {it + 1}, Reward: {episode_reward}")
 
@@ -128,8 +143,8 @@ if __name__ == '__main__':
         reward_per_episode.append(episode_reward)
 
     # test trained agent
-    testreward = tagent(model, env)
-    print(f"Average test reward: {testreward}")
+    test_reward = tagent(model, env)
+    print(f"Average test reward: {test_reward}")
 
     plt.plot(reward_per_episode)
     plt.title("Reward per episode")
